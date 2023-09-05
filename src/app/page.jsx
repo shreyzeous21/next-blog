b5f0502b4bb39fdc0dc2d1f81fe8505b46a6e971
Zeous_Shrey <shrey.sadhukhan21@gmail.com>
2023-09-05 22:47:12 +0530

    shrey

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+import styles from "./page.module.css"
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      {/* <Featured/> */}
+      {/* <CategoryList */}
+      <div className={styles.content}>
+        {/* <CardList /> */}
+        {/* <Menu/> */}
+      </div>
+    </div>
+  )
+}
